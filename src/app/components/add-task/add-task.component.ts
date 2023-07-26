61b70db76fa919b2d565274a56127a57cb264973
AbdulrehmanSuliman <abdulrehman.kassem00@eng-std.cu.edu.eg>
2023-07-26 21:31:11 +0300

    Task Tracker

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,44 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  text!: string;
+  day!: string;
+  showAddTask!: boolean;
+  subscription: Subscription;
+  reminder: boolean = false;
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((val) => (this.showAddTask = val));
+  }
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task!');
+      return;
+    }
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+    this.toggleAddTask();
+  }
+}
